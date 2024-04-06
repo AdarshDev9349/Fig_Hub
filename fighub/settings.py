8576b9efdce8cc78daa785c337b53599b5c5ec82
Adarshdev <adarshdev9349@gmail.com>
2024-04-06 12:01:34 +0530

    second commit

--- fighub/settings.py
+++ fighub/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='d8d91201658f40a0b862cef9fc2d3e51')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = []
 
