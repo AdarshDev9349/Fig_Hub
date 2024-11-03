b4b85658d3d15e48a75c3747bf54d6df175eb66f
Adarshdev <adarshdev9349@gmail.com>
2024-11-03 21:37:18 +0530

    error fix

--- fighub/settings.py
+++ fighub/settings.py
@@ -135,15 +135,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 import os
-STATIC_URL = 'static/'
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+STATIC_URL = '/static/'
+
 if not DEBUG:
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+else:
+    # Add STATIC_ROOT for development
+    STATIC_ROOT = BASE_DIR / 'staticfiles'  # Ensure this is set even in DEBUG mode for local testing
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
