ddd3105ce6a8dfba99d2baf6b65c7ae94a22bc10
Adarshdev <adarshdev9349@gmail.com>
2024-11-03 21:11:31 +0530

    error fix

--- fighub/settings.py
+++ fighub/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os ,dj_database_url
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -145,8 +151,3 @@ LOGIN_REDIRECT_URL = 'fig:home'
 LOGOUT_REDIRECT_URL= 'fig:login'
 
 
-import os
-from dotenv import load_dotenv
-
-# Load environment variables from .env file
-load_dotenv()
