5f314b8a2cf191ede21426fdf990bb8b4c8a1659
Adarshdev <adarshdev9349@gmail.com>
2024-11-12 19:06:46 +0530

    api fix

--- fig/figma.py
+++ fig/figma.py
@@ -1,13 +1,13 @@
-from urllib.parse import urlparse, parse_qs
-
 def extract_node_ids():
+    # Assuming '0:1' is the default node ID you need
     return '0:1'
 
 def extract_file_key(figma_link):
-
     parts = figma_link.split('/')
-    if len(parts) >= 5:
-        return parts[4]
-    else:
+    try:
+        if '/file/' in figma_link:
+            return parts[4] 
+        else:
+            return None
+    except IndexError:
         return None
-    
